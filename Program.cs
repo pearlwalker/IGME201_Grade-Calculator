d65c810cf982289835133d471f574db5adcf45d9
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-11-05 16:22:01 -0500

    Logic Error in "average" + Console edits
    Edited console messages for aesthetics. Shortened the "out of range" response to be closer in length to the other responses.
    
    Fixed a logic error I had made when calculating average. I noticed it when I changed a value in grades[] to be way out of range. Before, it added up all the values from grades[]. If you had any values in grades[] outside of 0-100, which is a value you aren't supposed to have, you can get an average outside of that range. I corrected this by making a separate variable  "values" to iterate thru grades[] and increase by one for each value in the 0-100 range. I also made average a float since you get decimal values with actual grades.

--- Program.cs
+++ Program.cs
@@ -18,61 +18,64 @@ internal class Program
             string studentname = "Pearl";
 
             // Declare 1D array, 10 integers long
-            int[] grades = {100, 92, 87, 200, -20, 52, 82, 75, 67, 88};
+            int[] grades = {100, 92, 87, 2000, -20, 52, 82, 75, 67, 88};
+
+            //Int to count grades values within range
+            int values = 0;
 
         // Declare average, init to 0
-        int average = 0;
+        float average = 0;
 
         //Greet student, give grades
         Console.WriteLine("WELCOME " + studentname + ", YOU WONT ACADEMIC YOU.");
-        Console.WriteLine("Here are your grades:");
+        Console.WriteLine("It seems you've taken " + grades.Length + " classes!");
 
         //Loop through grades[]
         foreach (int i in grades)
         {
 
-            //add grade value to var each iteration
-            average = average + i;
-            //Console.WriteLine("average + " + i + " = " + average);
-
             if (i <= 100 && i >= 90)
             {
-                //if grade is between 90 to 100 w nested if condition for 100%
-                Console.WriteLine(i + ": This grade is an A!");
+                
             if (i == 100)
-                {
-                    Console.Write(" And not only an A, but a perfect score!\n");
-                }
+                    {
+                        //if grade is between 90 to 100 w nested if condition for 100%
+                        Console.WriteLine(i + " is an A!");
+                        Console.Write("And not only an A, but a perfect score!\n");
+                } else
+                    {
+                        Console.WriteLine(i + " is an A!");
+                    }
 
             }
             else if (i < 90 && i >= 80)
             {
                 //if grade is between 80 and 90
-                Console.WriteLine(i + ": This grade is a B.");
+                Console.WriteLine(i + " is a B.");
 
             }
             else if (i < 80 && i >= 70)
             {
                 //if grade is between 70 and 80
-                Console.WriteLine(i + ": This grade is a C.");
+                Console.WriteLine(i + " is a C.");
 
             }
             else if (i < 70 && i >= 65)
             {
                 //if grade is between 65 and 70
-                Console.WriteLine(i + ": This grade is a D.");
+                Console.WriteLine(i + " is a D.");
 
             }
             else if (i < 65 && i >= 0)
             {
                 //if grade is between 65 and 0
-                Console.WriteLine(i + ": This grade is an F.");
+                Console.WriteLine(i + " is an F.");
 
             }
             else if (i > 100 || i < 0)
             {
                 //outside of the range of 0-100
-                Console.WriteLine(i + ": This grade is ouside of the range of 0-100...How did you get this grade??");
+                Console.WriteLine(i + " is...impossible?");
 
             }
             else
@@ -82,9 +85,23 @@ internal class Program
             }
 
         }
+        //calculate average, exclude outliers
+        foreach(int i in grades)
+            {
+                if (i <= 100 && i >= 0)
+                {
+                    Console.WriteLine(i);
+                    average = average + i;
+                    values++;
+                    Console.WriteLine(values);
+                } else
+                {
+                    //
+                }
+            }
 
         // get the average from average
-        average = average / 10;
+        average = average / values;
 
         Console.WriteLine("Well " + studentname + ", it seems your average grade is " + average + ".");
         if (average == 100)
